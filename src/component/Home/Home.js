f7fe6b7ef5a8b9246fa950b338ac1732c087fc8e
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-09-19 03:06:20 +0600

    home page cart done with responsiveness

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -8,6 +8,122 @@ const Home = () => {
         <i class="fa-solid fa-user"></i>
         <button>Create an Account</button>
       </Link>
+      <div className="home-parent-cart-div">
+        <div className="home-single-cart-parent-div">
+          <span className="featured">Featured</span>
+          <div className="home-single-cart-div">
+            <img
+              className="home-cart-photo"
+              src="https://i0.wp.com/www.entrepreneurs.ng/wp-content/uploads/2022/04/How-to-write-a-company-profile.jpg?fit=1200%2C675&ssl=1"
+              alt=""
+            />
+            <div className="home-cart-text-div">
+              <h2 className="companyName">
+                Company Name <sup className="sup-open">Open</sup>
+              </h2>
+              <div className="location-rating-div">
+                <span className="location-span">
+                  <i class="fa-solid fa-location-dot"></i>
+                  <span>Location Address</span>
+                </span>
+                <span className="rating-span">
+                  <i class="fa-solid fa-star"></i>
+                  <span>4.8 Rating</span>
+                </span>
+              </div>
+            </div>
+
+            <div className="home-i-div">
+              <i class="fa-solid fa-phone"></i>
+            </div>
+          </div>
+        </div>
+        <div className="home-single-cart-parent-div">
+          <span className="Top1inAPB">Top # 1 in APB</span>
+          <div className="home-single-cart-div border-second">
+            <img
+              className="home-cart-photo"
+              src="https://i0.wp.com/www.entrepreneurs.ng/wp-content/uploads/2022/04/How-to-write-a-company-profile.jpg?fit=1200%2C675&ssl=1"
+              alt=""
+            />
+            <div className="home-cart-text-div">
+              <h2 className="companyName">
+                Company Name <sup className="sup-open">Open</sup>
+              </h2>
+              <div className="location-rating-div">
+                <span className="location-span">
+                  <i class="fa-solid fa-location-dot"></i>
+                  <span>Location Address</span>
+                </span>
+                <span className="rating-span">
+                  <i class="fa-solid fa-star"></i>
+                  <span>4.8 Rating</span>
+                </span>
+              </div>
+            </div>
+
+            <div className="home-i-div">
+              <i class="fa-solid fa-phone"></i>
+            </div>
+          </div>
+        </div>
+        <div className="home-single-cart-parent-div">
+          <div className="home-single-cart-div border-three">
+            <img
+              className="home-cart-photo"
+              src="https://i0.wp.com/www.entrepreneurs.ng/wp-content/uploads/2022/04/How-to-write-a-company-profile.jpg?fit=1200%2C675&ssl=1"
+              alt=""
+            />
+            <div className="home-cart-text-div">
+              <h2 className="companyName">
+                Company Name <sup className="sup-open">CLOSE</sup>
+              </h2>
+              <div className="location-rating-div">
+                <span className="location-span">
+                  <i class="fa-solid fa-location-dot"></i>
+                  <span>Location Address</span>
+                </span>
+                <span className="rating-span">
+                  <i class="fa-solid fa-star"></i>
+                  <span>4.8 Rating</span>
+                </span>
+              </div>
+            </div>
+
+            <div className="home-i-div">
+              <i class="fa-solid fa-phone"></i>
+            </div>
+          </div>
+        </div>
+        <div className="home-single-cart-parent-div">
+          <div className="home-single-cart-div border-three">
+            <img
+              className="home-cart-photo"
+              src="https://i0.wp.com/www.entrepreneurs.ng/wp-content/uploads/2022/04/How-to-write-a-company-profile.jpg?fit=1200%2C675&ssl=1"
+              alt=""
+            />
+            <div className="home-cart-text-div">
+              <h2 className="companyName">
+                Company Name <sup className="sup-open">Open</sup>
+              </h2>
+              <div className="location-rating-div">
+                <span className="location-span">
+                  <i class="fa-solid fa-location-dot"></i>
+                  <span>Location Address</span>
+                </span>
+                <span className="rating-span">
+                  <i class="fa-solid fa-star"></i>
+                  <span>4.8 Rating</span>
+                </span>
+              </div>
+            </div>
+
+            <div className="home-i-div">
+              <i class="fa-solid fa-phone"></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
